ad95e56cb3a4c3f162d6fda2320cb1c3dadb474b
ashu <ashishsoni2002@gmail.com>
2024-02-06 23:43:45 +0530

    completing the foundation

--- App.js
+++ App.js
@@ -3,7 +3,56 @@ import React from 'react'
 function App() {
   return (
     <h1>App</h1>
-  )
+
+
+    // React.createElement('h1', null, 'App') is objet -> it renders -> and then we got htmlElement()
+    
+
+    // JSX - is not HTML in js - it merges HTML and JS together 
+    // React and jsx is different 
+    // JSX is html like syntax (liiks like html, xml)  but it is transformed into regular javascript object by babel or typescript compiler
+    
+    
+    // code is written for both human and computer 
+    // first for humans bcz they read and then for computer
+    
+    // js engine understand ES6 
+    // JSX is not understood by js engine
+    // so we need to convert JSX into js object
+    // Babel is a tool that converts JSX into js object
+    // Babel is a transpiler
+    // Babel is a compiler
+    // Babel is a source-to-source compiler
+    
+    // JSX is transpiled before it reaches the JS engine
+
+    
+    // babel understand JSX and babel converts JSX into js object (readable by react)
+
+    // JSX -> Babel  -> React.createElement -> JS object -> HTML element
+  
+  
+  
+  
+  
+  // React Components
+  // React components are the building blocks of any React application
+  // 2 types of componennts in react -  class based (old way) , functional components;
+  
+  // React Functional component - returns jsx code 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    )
 }
 
 export default App
